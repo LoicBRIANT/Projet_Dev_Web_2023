d8a19e5466f2df82aedd2d3c0d56892acb955d44
LoicBRIANT <briantloic@cy-tech.fr>
2023-02-26 23:30:57 +0100

    J ai remplie un peut le index.php et le css

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php?>
+
+<!DOCTYPE html>
+<html>
+
+<head lang="fr">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Zaun|Magasin spécialisé dans la vente de champignon</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+    <div>blabla</div>
+</body>
+</html>
