ddd2f0864a59355fbf3b65af05586c3d80fb3c88
LoicBRIANT <briantloic@cy-tech.fr>
2023-03-28 01:10:34 +0200

    Le gros du header

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<head>
+  <link rel="stylesheet" href="css/header.css">
+</head>
 <header>
     <img id="icon_zaun" src="/img/zaun_crest_icon.png" alt="zaun_icon">
     <div id="titre_menu">
@@ -5,7 +8,13 @@
     </div>
     <div class="search-container">
     <form action="index.php">
-      <input type="text" placeholder="Search.." name="search"><button type="submit"><img src="img/icons8-search-50_brown.png" class="search_button" alt="search_icon></button>
+      <input type="text" placeholder="Search.." name="search"><div id="search"><button type="submit"><img src="/img/icons8-search-50.png" class="search_button" alt="search_icon"></button></div>
     </form>
-  </div>
+    </div>
+    <div id="compte">
+      <p>Compte</p>
+    </div>
+    <div id="shop">
+      <img src="/img/shopping-cart.png" alt="shopping_cart_icon" id="shop_button">
+    </div>
 </header>
