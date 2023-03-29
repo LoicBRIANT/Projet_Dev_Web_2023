cd6d1f322dfd3a0fef13e893ace034941a3bbe99
Solène <120031563+rootcipher0@users.noreply.github.com>
2023-03-29 02:50:31 +0200

    Update and rename acheteur.html to acheteur.php

--- acheteur.php
+++ acheteur.php
@@ -1,30 +1,41 @@
-<!DOCTYPE html>
-<html>
     <head>
-        <titre>Page_vendeur</titre>
+        <br>
+        <titre>Page_achteur</titre>
+        <link rel="stylesheet" type="text/css" href="style_acheteur.css">
+        <br>
         <br>
     </head>  
 
     <body>
-        <p>Image du profil</p>
+        <div class="carre"></div>
+        <br>
         <p> Informations de l'utilisateur </p>
         <br>
+        <br>
         <p>-identifiant/adresse mail/téléphone</p>
+        <br>
         <p>-nom de l'entrerpise ou statut particulier</p>
 
-        <div class="input-container-form ic1">
+        <div class="input-container-form">
+            <div class=pani>
             <button type="text" class="Panier">Panier</button>
+            </div>
         </div>
 
-         <div class="input-container-form ic1">
+         <div class="input-container-form">
+            <div class="s_abon">
             <button type="text" class="S'abonner">S'abonner</button>
+            </div>
          </div>
-         <div class="input-container-form ic1">
+         <div class="input-container-form">
+            <div class="comm">
             <button type="text" class="Commande">Commande</button>
+            </div>
          </div>
 
-         <div class="input-container-form ic1">
+         <div class="input-container-form">
+            <div class="complet">
             <button type="text" class="Compléter des informations">Compléter des informations</button>
+            </div>
          </div>
     </body>
-</html>
