0039fe8cda7af94b05cb85f509717bb03a12879b
LoicBRIANT <briantloic@cy-tech.fr>
2023-03-29 02:12:49 +0200

    transformer qq fichier en php et g rajouter le menu du cote

---
+++ admin_menu.php
@@ -0,0 +1,30 @@
+
+<html>  
+    <head>        
+        <meta charset="utf-8" />        
+        <meta name="viewport" content="width=device-width"/> 
+        <br>    
+        <br>    
+        <title>Menu_de_l'administrateur
+        </title> 
+        <link rel="stylesheet" type="text/css" href="css/style_admin_menu.css">
+    </head> 
+
+        <br>
+
+    <body>
+
+
+        <div class="title">Journal des activités</div>
+        <br>
+        <div class="ligne"></div>
+        <pre> Action            heure           utilisateur concerné</pre> 
+        <br>
+        <div class="ligne"></div>
+        <pre> Action            heure           utilisateur concerné</pre>
+        <br>
+        <div class="ligne"></div>
+        <pre> Action            heure           utilisateur concerné</pre>
+        <br>
+    </body>
+
