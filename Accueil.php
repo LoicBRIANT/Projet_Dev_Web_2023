1b6f6a283b0fa535ee2f4c032d29bbd582858441
LoicBRIANT <120270606+LoicBRIANT@users.noreply.github.com>
2023-03-30 22:30:02 +0200

    Add files via upload
    2 fichier Accueil et Commandes

---
+++ Accueil.php
@@ -0,0 +1,51 @@
+<head>
+  <link rel="stylesheet" href="css/Accueil.css">
+    <title>Zaun|Magasin spécialisé dans la vente de champignon</title>
+</head>
+<div id="Accueil">
+    <div class="Produit">
+        <img src="img/christmas_beth.png" alt="best waifu">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+    <div class="Produit">
+        <img src="img/christmas_beth.png">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+    <div class="Produit">
+        <img src="img/christmas_beth.png">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+    <div class="Produit">
+        <img src="img/christmas_beth.png">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+    <div class="Produit">
+        <img src="img/christmas_beth.png">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+    <div class="Produit">
+        <img src="img/christmas_beth.png">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+    <div class="Produit">
+        <img src="img/christmas_beth.png">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+    <div class="Produit">
+        <img src="img/christmas_beth.png">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+    <div class="Produit">
+        <img src="img/christmas_beth.png">
+        <div class="nom_produit">Christmas Beth</div>
+        <div class="Prix">200€</div>
+    </div>
+</div>
