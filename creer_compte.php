01e8264f84e63992356a7d344d22f9d6045cb309
Solène <120031563+rootcipher0@users.noreply.github.com>
2023-04-26 23:21:51 +0200

    Add files via upload

---
+++ creer_compte.php
@@ -0,0 +1,132 @@
+<?php?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <br>
+        <title>Creation_compte</title>
+        <link rel="stylesheet" type="text/css" href="css/creation_compte.css">
+        <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="css/side_menu.css">
+        <link rel="stylesheet" href="css/footer.css">
+        <br>
+        <br>
+    </head>  
+
+    <body>
+
+        <?php include('header.php'); ?>
+        <?php include('side_menu.php'); ?>
+
+        <div class="rectangle">
+            <form id="form">
+                <div class="title">Création d'un compte</div>
+                <br>
+                <div class="input-container ic1">
+                    <label for="Entrer votre prénom" class="placeholder">Prénom : </label>
+                    <br>
+                    <input id="prenom" class="input" type="text" placeholder="Entrer votre prénom " />
+                    <div class="cut"></div>
+                </div>
+                <br>
+                <div class="input-container ic1">
+                    <label for="Entrer votre nom" class="placeholder">Nom : </label>
+                    <br>
+                    <input id="nom" class="input" type="text" placeholder="Entrer votre nom" />
+                    <div class="cut"></div>
+                </div>
+                <br>
+                <div class="input-container ic1">
+                    <label for="Entrer une Adresse e-mail" class="placeholder">Adresse e-mail : </label>
+                    <br>
+                    <input id="Adresse e-mail" class="input" type="text" placeholder="Entrer votre adresse e-mail " />
+                    <div class="cut"></div>
+                </div>
+                <br>
+                <div class="input-container ic1">
+                    <label for="Entrer un identifiant" class="placeholder">Identifiant : </label>
+                    <br>
+                    <input id="Adresse e-mail" class="input" type="text" placeholder="Entrer votre identifiant " />
+                    <div class="cut"></div>
+                </div>
+                <br>
+                <div class="input-container ic1">
+                        <label for="Entrer votre Mot de passe" class="placeholder">Entrer votre mot de passe :</label>
+                        <br>
+                        <input id="Mot de passe" class="input" type="text" placeholder="Entrer votre mot de passe " />
+                        <div class="cut"></div>
+                </div>
+                <br>
+                <div class="input-container ic1">
+                    <label for="Confirmer votre Mot de passe" class="placeholder">Confirmer votre mot de passe :</label>
+                    <br>
+                    <input id="Confirmer Mot de passe" class="input" type="text" placeholder="Confirmer votre mot de passe " />
+                    <div class="cut"></div>
+            </div>
+        
+                <br><br>
+                <br>
+                <br>
+                <br>
+                    <div class="input-container-form">
+                        <div class="connec">
+                            <button type="submit" class="Création_d'un_compte">Créer</button>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="input-container-form">
+                        <div class="compt">
+                            <button type="button" class="Retour vers l'accueil">
+                                <a href=Accueil.html>Retour vers l'accueil</a>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="input-container-form">  
+                        <div class ="retour">
+                            <button type="button" class="Retour vers login">
+                                <a href=login.html>Retour vers page de Login</a>
+                            </button>
+                        </div>
+                    </div>
+            </form>
+        </div>
+        
+        <?php include('footer.php'); ?>
+    </body>
+</html>
+
+<?php
+
+// Vérifier si le formulaire a été soumis
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Récupérer les données du formulaire
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $identifiant = $_POST['identifiant'];
+    $mot_de_passe = $_POST['mot_de_passe'];
+
+    // Connexion à la base de données
+    $connexion = mysqli_connect('localhost', 'utilisateur', 'mot_de_passe', 'Scriptsql_BDD.SQL');
+
+    // Vérifier si la connexion a réussi
+    if (!$connexion) {
+        die('Erreur de connexion à la base de données : ' . mysqli_connect_error());
+    }
+
+    // Préparer la requête d'insertion des données dans la table Compte
+    $requete = "INSERT INTO Compte (prenom, nom, email, identifiant, mot_de_passe) VALUES ('$prenom', '$nom', '$email', '$identifiant', '$mot_de_passe')";
+
+    // Exécuter la requête
+    if (mysqli_query($connexion, $requete)) {
+        echo 'Le compte a été créé avec succès.';
+    } else {
+        echo 'Erreur lors de la création du compte : ' . mysqli_error($connexion);
+    }
+
+    // Fermer la connexion
+    mysqli_close($connexion);
+}
+
+?>
+    
